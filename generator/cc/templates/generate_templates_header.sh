23d7853a6f9b445e816346a01eea56c9689452d9
Kamran Amini <kam.cpp@gmail.com>
2015-12-03 02:38:19 +0330

    Templates are embedded and bugs are fixed.

--- generator/cc/templates/generate_templates_header.sh
+++ generator/cc/templates/generate_templates_header.sh
@@ -1,12 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "" > $1/templates.cc
+
+for f in `find . -name "*.template"`; do
+  xxd -i $f >> $1/templates.cc
+done
 
 echo "" > $1/templates.h
+
 echo "#ifndef _NAEEM_HOTTENTOT_GENERATOR__TEMPLATES_H_" >> $1/templates.h
 echo "#define _NAEEM_HOTTENTOT_GENERATOR__TEMPLATES_H_" >> $1/templates.h
 echo "" >> $1/templates.h
 
-for f in `find . -name "*.template"`; do
-  xxd -i $f >> $1/templates.h
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for f in `cat $1/templates.cc | grep unsigned | cut -d'=' -f1`; do
+  echo "extern $f;" >> $1/templates.h
 done
+IFS=$SAVEIFS
 
+echo "" >> $1/templates.h
 echo "#endif" >> $1/templates.h
