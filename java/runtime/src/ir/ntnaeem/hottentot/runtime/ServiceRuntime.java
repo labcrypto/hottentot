0a04bf9d673e7afa32110b023328b9585de330a7
alisharifi <alisharifi01@gmail.com>
2016-01-24 17:08:19 +0330

    minor changes

---
+++ java/runtime/src/ir/ntnaeem/hottentot/runtime/ServiceRuntime.java
@@ -0,0 +1,68 @@
+/*  The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 Noavaran Tejarat Gostar NAEEM Co.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTAB_STRILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package ir.ntnaeem.hottentot.runtime;
+
+import ir.ntnaeem.hottentot.runtime.exception.HottentotRuntimeException;
+import ir.ntnaeem.hottentot.runtime.factory.TcpServerFactory;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ServiceRuntime {
+
+    private Map<Endpoint,List<Service>> serviceMap = new HashMap<Endpoint, List<Service>>();
+    private Map<Endpoint , Map<Long , RequestHandler>> requestHandlerMap = new HashMap<Endpoint, Map<Long, RequestHandler>>();
+    public void register(String host , int port , Service service){
+        Endpoint endPoint = new Endpoint(host,port);
+        if(serviceMap.containsKey(endPoint)){
+            List<Service> serviceListInEndpoint = serviceMap.get(endPoint);
+            //TODO search in service list for duplicate service registered
+            serviceListInEndpoint.add(service);
+            Map<Long, RequestHandler> requestHandlerMapInEndpoint = requestHandlerMap.get(endPoint);
+            requestHandlerMapInEndpoint.put(service.getServiceId() , service.makeRequestHandler());
+        }else{
+            ArrayList<Service> serviceList = new ArrayList<Service>();
+            serviceList.add(service);
+            serviceMap.put(endPoint,serviceList);
+            HashMap<Long, RequestHandler> requestHandlers = new HashMap<Long , RequestHandler>();
+            requestHandlers.put(service.getServiceId() , service.makeRequestHandler());
+            requestHandlerMap.put(endPoint, requestHandlers);
+        }
+    }
+    public void start(){
+        for(Map.Entry<Endpoint , Map<Long , RequestHandler> > entry : requestHandlerMap.entrySet()){
+            Map<Long, RequestHandler> requestHandlerMapInEndpoint = entry.getValue();
+            Endpoint endpoint = entry.getKey();
+            TcpServer tcpServer = TcpServerFactory.create(endpoint.getHost(), endpoint.getPort(), requestHandlerMapInEndpoint);
+            try {
+                tcpServer.bindAndStart();
+            } catch (IOException e) {
+                throw new HottentotRuntimeException(e);
+            }
+        }
+        while(true);
+    }
+}
---
+++ java/runtime/src/ir/ntnaeem/hottentot/serializerHelper/PDTDeserializer.java
@@ -0,0 +1,118 @@
+/*  The MIT License (MIT)
+ *
+ *  Copyright (c) 2015 Noavaran Tejarat Gostar NAEEM Co.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTAB_STRILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+package ir.ntnaeem.hottentot.serializerHelper;
+
+import com.sun.org.apache.xalan.internal.xsltc.dom.SortingIterator;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import static java.lang.StrictMath.pow;
+
+public class PDTDeserializer {
+
+  public static void main(String[] args) {
+    String string = getString(new byte[]{97 , 98 , 99});
+    String str = "س";
+    byte[] bytes = str.getBytes();
+  }
+
+  public static String getFullString(byte[] bytes) {
+    //TODO notice utf8 !
+    int dataLength = 0;
+    int counter = 0;
+    if((bytes[0] & 0x80) == 0) {
+      dataLength = bytes[counter++];
+    }else {
+      int numbersOfBytesForDataLength = bytes[0] & 0x0f;
+      for(byte i = 0 ; i < numbersOfBytesForDataLength ; i++){
+        dataLength += pow(256, numbersOfBytesForDataLength - i - 1) * bytes[counter++];
+      }
+    }
+    byte[] valueByteArray = new byte[dataLength];
+    System.arraycopy(bytes,counter,valueByteArray,0,dataLength);
+    return new String(valueByteArray);
+  }
+  public static String getString(byte[] bytes){
+    return new String(bytes);
+  }
+
+  public static boolean getBool(byte b) {
+    if (b == 1) {
+      return true;
+    }
+    return false;
+  }
+
+  public static byte getInt8(byte[] bytes) {
+    return bytes[0];
+  }
+
+  public static short getUint16(byte[] bytes) {
+    short number = 0;
+    for (byte i = 0; i < bytes.length; i++) {
+      if (bytes[i] < 0) {
+        number += pow(256, bytes.length - 1 - i) * (bytes[i] + 256);
+      } else {
+        number += pow(256, bytes.length - 1 - i) * bytes[i];
+      }
+    }
+    return number;
+  }
+
+  public static short getInt16(byte[] bytes) {
+    return  ByteBuffer.allocate(2).wrap(bytes).getShort();
+  }
+
+  public static int getUint32(byte[] bytes) {
+    int number = 0;
+    for (byte i = 0; i < bytes.length; i++) {
+      if (bytes[i] < 0) {
+        number += pow(256, bytes.length - 1 - i) * (bytes[i] + 256);
+      } else {
+        number += pow(256, bytes.length - 1 - i) * bytes[i];
+      }
+    }
+    return number;
+  }
+
+  public static int getInt32(byte[] bytes) {
+    return  ByteBuffer.allocate(4).wrap(bytes).getInt();
+  }
+
+  public static long getUint64(byte[] bytes) {
+    long number = 0L;
+    for (byte i = 0; i < bytes.length; i++) {
+      if (bytes[i] < 0) {
+        number += pow(256, bytes.length - 1 - i) * (bytes[i] + 256);
+      } else {
+        number += pow(256, bytes.length - 1 - i) * bytes[i];
+      }
+    }
+    return number;
+  }
+
+  public static long getInt64(byte[] bytes) {
+    return  ByteBuffer.allocate(8).wrap(bytes).getLong();
+  }
+}
