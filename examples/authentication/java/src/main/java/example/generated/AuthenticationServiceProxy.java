ccd280a9f7f36cce684c5c42fc47029e714ea39c
alisharifi <alisharifi01@gmail.com>
2016-02-03 21:55:32 +0330

    java runtime : server closed client socket after sending reposne

--- examples/authentication/java/src/main/java/example/generated/AuthenticationServiceProxy.java
+++ examples/authentication/java/src/main/java/example/generated/AuthenticationServiceProxy.java
@@ -96,6 +96,7 @@ public class AuthenticationServiceProxy extends AbstractAuthenticationService im
     }
     //deserialize token part of response
     Response response = protocol.getResponse();
+
     //close everything
      try { 
        tcpClient.close(); 
