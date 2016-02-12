7c3c0ca93b7c977dd534a56cdaa94d1346191d4d
Kamran Amini <kam.cpp@gmail.com>
2016-02-12 12:41:17 +0330

    c.sh file is removed.

--- c.sh
+++
@@ -1,7 +0,0 @@
-git filter-branch -f --commit-filter '
-    if [ "$GIT_AUTHOR_EMAIL" = "kamcpp@gmail.com" ];
-    then
-            skip_commit "$@";
-    else
-            git commit-tree "$@";
-    fi' HEAD
