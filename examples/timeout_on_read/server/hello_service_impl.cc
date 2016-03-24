c06766316f40a1326651b3d53fae8e1b6aeaf9e0
Kamran Amini <kam.cpp@gmail.com>
2016-03-25 00:24:02 +0430

    Server down event is not detected. Sample source code has been added to proxy in example.

--- examples/timeout_on_read/server/hello_service_impl.cc
+++ examples/timeout_on_read/server/hello_service_impl.cc
@@ -45,7 +45,7 @@ namespace timeout_on_read {
     }
     out = response;
     // throw std::runtime_error("Exception is occurred!");
-    // int x = 5 / 0;
+    int x = 5 / 0;
   }
 } // END OF NAMESPACE timeout_on_read
 } // END OF NAMESPACE examples
