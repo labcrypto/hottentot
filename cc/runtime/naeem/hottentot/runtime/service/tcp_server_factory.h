bbf26cd9fa90f83dfcb012b42c0b3b59666f3e8b
Kamran Amini <kam.cpp@gmail.com>
2016-04-26 11:19:27 +0430

    Licence header has been changed in all files.

--- cc/runtime/naeem/hottentot/runtime/service/tcp_server_factory.h
+++ cc/runtime/naeem/hottentot/runtime/service/tcp_server_factory.h
@@ -1,6 +1,6 @@
 /*  The MIT License (MIT)
  *
- *  Copyright (c) 2015 Noavaran Tejarat Gostar NAEEM Co.
+ *  Copyright (c) 2015 LabCrypto Organization
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
