85065dcd93aa0cb8ae47acae1d85ff53112e459f
Kamran Amini <kam.cpp@gmail.com>
2016-04-30 12:43:33 +0430

    Namespaces are being changed.

--- cc/runtime/naeem/hottentot/runtime/proxy/default_tcp_client.cc
+++ cc/runtime/naeem/hottentot/runtime/proxy/default_tcp_client.cc
@@ -1,6 +1,6 @@
 /*  The MIT License (MIT)
  *
- *  Copyright (c) 2015 Noavaran Tejarat Gostar NAEEM Co.
+ *  Copyright (c) 2015 LabCrypto Org.
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -61,180 +61,182 @@ typedef unsigned __int64 uint64_t;
 #include "../utils.h"
 
 
-namespace naeem {
-  namespace hottentot {
-    namespace runtime {
-      namespace proxy {
-        DefaultTcpClient::DefaultTcpClient(std::string host,
-                                           uint32_t port)
-            : TcpClient(host, port),
-              socketFD_(0) {
-        }
-        DefaultTcpClient::~DefaultTcpClient() {
-        }
-        bool 
-        DefaultTcpClient::Connect() {
+namespace org {
+namespace labcrypto {
+namespace hottentot {
+namespace runtime {
+namespace proxy {
+  DefaultTcpClient::DefaultTcpClient(std::string host,
+                                     uint32_t port)
+      : TcpClient(host, port),
+        socketFD_(0) {
+  }
+  DefaultTcpClient::~DefaultTcpClient() {
+  }
+  bool 
+  DefaultTcpClient::Connect() {
 #ifndef _MSC_VER
-          struct sockaddr_in serverAddr;
-          struct hostent *server;
-          socketFD_ = socket(AF_INET, SOCK_STREAM, 0);
-          if (socketFD_ < 0) {
-            if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-              std::cerr << 
-                "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
-                  "ERROR opening socket" << std::endl;
-            }
-            // exit(1);
-            return false;
-          }
-          server = gethostbyname(host_.c_str());
-          if (server == NULL) {
-            if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-              std::cerr << 
-                "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
-                  "ERROR, no such host" << std::endl;
-            }
-            close(socketFD_);
-            // exit(1);
-            return false;
-          }
-          memset((char *) &serverAddr, 0, sizeof(serverAddr));
-          serverAddr.sin_family = AF_INET;
-          // bcopy((char *)&serverAddr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
-          serverAddr.sin_port = htons(port_);
-          if (inet_pton(AF_INET, host_.c_str(), &serverAddr.sin_addr) <= 0) {
-            if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-              std::cerr << 
-                "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
-                  "ERROR setting host" << std::endl;
-            }
-            close(socketFD_);
-            // exit(1);
-            return false;
-          }
-          if (::naeem::hottentot::runtime::Configuration::SocketReadTimeout() > 0) {
-            struct timeval tv;
-            tv.tv_sec = ::naeem::hottentot::runtime::Configuration::SocketReadTimeout();
-            tv.tv_usec = 0;
-            if (setsockopt(socketFD_, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval)) < 0) {
-              if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-                std::cerr << 
-                  "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
-                    "ERROR setting read timeout." << std::endl;
-              }
-              close(socketFD_);
-              // exit(1);
-              return false;
-            }
-          }
-          if (connect(socketFD_, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
-            if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-              std::cerr << 
-                "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
-                  "ERROR connecting" << std::endl;
-            }
-            close(socketFD_);
-            // exit(1);
-            return false;
-          }
-#else
-          WSADATA wsaData;
-          struct addrinfo *result = NULL,
-                          hints;
-          // Initialize Winsock
-          int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-          if (iResult != 0) {
-              printf("WSAStartup failed with error: %d\n", iResult);
-              return false;
-          }
-          ZeroMemory(&hints, sizeof(hints));
-          hints.ai_family = AF_UNSPEC;
-          hints.ai_socktype = SOCK_STREAM;
-          hints.ai_protocol = IPPROTO_TCP;
-          // Resolve the server address and port
-          std::stringstream ss;
-          ss << port_;
-          iResult = getaddrinfo(host_.c_str(), ss.str().c_str(), &hints, &result);
-          if ( iResult != 0 ) {
-              printf("getaddrinfo failed with error: %d\n", iResult);
-              WSACleanup();
-              return false;
-          }
-          // Create a SOCKET for connecting to server
-          socketFD_ = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-          if (socketFD_ == INVALID_SOCKET) {
-              printf("socket failed with error: %ld\n", WSAGetLastError());
-              WSACleanup();
-              return false;
-          }
-          // Set recv timeout
-          if (::naeem::hottentot::runtime::Configuration::SocketReadTimeout() > 0) {
-            int nTimeout = ::naeem::hottentot::runtime::Configuration::SocketReadTimeout() * 1000;
-            if (setsockopt(socketFD_, SOL_SOCKET, SO_RCVTIMEO, (const char*)&nTimeout, sizeof(int)) != 0) {
-              printf("setsockopt failed with error: %ld\n", WSAGetLastError());
-              WSACleanup();
-              return false;
-            }
-          }
-          // Connect to server.
-          iResult = connect(socketFD_, result->ai_addr, (int)result->ai_addrlen);
-          if (iResult == SOCKET_ERROR) {
-            closesocket(socketFD_);
-            socketFD_ = INVALID_SOCKET;
-            return false;
-          }
-          freeaddrinfo(result);
-#endif
-          return true;
+    struct sockaddr_in serverAddr;
+    struct hostent *server;
+    socketFD_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD_ < 0) {
+      if (::naeem::hottentot::runtime::Configuration::Verbose()) {
+        std::cerr << 
+          "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
+            "ERROR opening socket" << std::endl;
+      }
+      // exit(1);
+      return false;
+    }
+    server = gethostbyname(host_.c_str());
+    if (server == NULL) {
+      if (::naeem::hottentot::runtime::Configuration::Verbose()) {
+        std::cerr << 
+          "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
+            "ERROR, no such host" << std::endl;
+      }
+      close(socketFD_);
+      // exit(1);
+      return false;
+    }
+    memset((char *) &serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    // bcopy((char *)&serverAddr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+    serverAddr.sin_port = htons(port_);
+    if (inet_pton(AF_INET, host_.c_str(), &serverAddr.sin_addr) <= 0) {
+      if (::naeem::hottentot::runtime::Configuration::Verbose()) {
+        std::cerr << 
+          "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
+            "ERROR setting host" << std::endl;
+      }
+      close(socketFD_);
+      // exit(1);
+      return false;
+    }
+    if (::naeem::hottentot::runtime::Configuration::SocketReadTimeout() > 0) {
+      struct timeval tv;
+      tv.tv_sec = ::naeem::hottentot::runtime::Configuration::SocketReadTimeout();
+      tv.tv_usec = 0;
+      if (setsockopt(socketFD_, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval)) < 0) {
+        if (::naeem::hottentot::runtime::Configuration::Verbose()) {
+          std::cerr << 
+            "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
+              "ERROR setting read timeout." << std::endl;
         }
-        void 
-        DefaultTcpClient::Write(unsigned char *data,
-                                uint32_t       dataLength) {
-          if (dataLength == 0) {
-            return;
-          }
-#ifndef _MSC_VER
-          int result = write(socketFD_, data, dataLength * sizeof(unsigned char));
-          if (result <= 0) {
-            throw std::runtime_error("[" + ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() + "]: Write to service failed.");
-          }
-          /* for (uint32_t i = 0; i < dataLength; i++) {
-            std::cout << "Writing ..." << std::endl;
-            int result = write(socketFD_, &data[i], sizeof(unsigned char));
-            std::cout << "Write result: " << result << std::endl;
-            if (result <= 0) {
-              throw std::runtime_error("Write to service failed.");
-            }
-            sleep(2);
-          } */
-#else
-          int result = send(socketFD_, (char *)data, dataLength * sizeof(unsigned char), 0);
-          if (result == SOCKET_ERROR) {
-            throw std::runtime_error("[" + ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() + "]: Write to service failed.");
-          }
-#endif
-        }
-        uint32_t 
-        DefaultTcpClient::Read(unsigned char *buffer,
-                               uint32_t bufferLength) {
-#ifndef _MSC_VER
-          return read(socketFD_, buffer, bufferLength * sizeof(unsigned char));
-#else
-          return recv(socketFD_, (char *)buffer, bufferLength * sizeof(unsigned char), 0);
-#endif
-        }
-        void 
-        DefaultTcpClient::Close() {
-          if (socketFD_ > 0) {
-#ifndef _MSC_VER
-            close(socketFD_);
-#else
-            closesocket(socketFD_);
-#endif
-            socketFD_ = 0;
-          }
-        }
+        close(socketFD_);
+        // exit(1);
+        return false;
       }
     }
+    if (connect(socketFD_, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+      if (::naeem::hottentot::runtime::Configuration::Verbose()) {
+        std::cerr << 
+          "[" << ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() << "]: " <<
+            "ERROR connecting" << std::endl;
+      }
+      close(socketFD_);
+      // exit(1);
+      return false;
+    }
+#else
+    WSADATA wsaData;
+    struct addrinfo *result = NULL,
+                    hints;
+    // Initialize Winsock
+    int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return false;
+    }
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    // Resolve the server address and port
+    std::stringstream ss;
+    ss << port_;
+    iResult = getaddrinfo(host_.c_str(), ss.str().c_str(), &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return false;
+    }
+    // Create a SOCKET for connecting to server
+    socketFD_ = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (socketFD_ == INVALID_SOCKET) {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return false;
+    }
+    // Set recv timeout
+    if (::naeem::hottentot::runtime::Configuration::SocketReadTimeout() > 0) {
+      int nTimeout = ::naeem::hottentot::runtime::Configuration::SocketReadTimeout() * 1000;
+      if (setsockopt(socketFD_, SOL_SOCKET, SO_RCVTIMEO, (const char*)&nTimeout, sizeof(int)) != 0) {
+        printf("setsockopt failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return false;
+      }
+    }
+    // Connect to server.
+    iResult = connect(socketFD_, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+      closesocket(socketFD_);
+      socketFD_ = INVALID_SOCKET;
+      return false;
+    }
+    freeaddrinfo(result);
+#endif
+    return true;
   }
+  void 
+  DefaultTcpClient::Write(unsigned char *data,
+                          uint32_t       dataLength) {
+    if (dataLength == 0) {
+      return;
+    }
+#ifndef _MSC_VER
+    int result = write(socketFD_, data, dataLength * sizeof(unsigned char));
+    if (result <= 0) {
+      throw std::runtime_error("[" + ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() + "]: Write to service failed.");
+    }
+    /* for (uint32_t i = 0; i < dataLength; i++) {
+      std::cout << "Writing ..." << std::endl;
+      int result = write(socketFD_, &data[i], sizeof(unsigned char));
+      std::cout << "Write result: " << result << std::endl;
+      if (result <= 0) {
+        throw std::runtime_error("Write to service failed.");
+      }
+      sleep(2);
+    } */
+#else
+    int result = send(socketFD_, (char *)data, dataLength * sizeof(unsigned char), 0);
+    if (result == SOCKET_ERROR) {
+      throw std::runtime_error("[" + ::naeem::hottentot::runtime::Utils::GetCurrentUTCTimeString() + "]: Write to service failed.");
+    }
+#endif
+  }
+  uint32_t 
+  DefaultTcpClient::Read(unsigned char *buffer,
+                         uint32_t bufferLength) {
+#ifndef _MSC_VER
+    return read(socketFD_, buffer, bufferLength * sizeof(unsigned char));
+#else
+    return recv(socketFD_, (char *)buffer, bufferLength * sizeof(unsigned char), 0);
+#endif
+  }
+  void 
+  DefaultTcpClient::Close() {
+    if (socketFD_ > 0) {
+#ifndef _MSC_VER
+      close(socketFD_);
+#else
+      closesocket(socketFD_);
+#endif
+      socketFD_ = 0;
+    }
+  }
+}
+}
+}
+}
 }
