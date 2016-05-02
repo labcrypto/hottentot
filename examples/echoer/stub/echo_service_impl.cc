8f95d72aab5de07c8f7ed37881413bb04fc42602
Kamran Amini <kam.cpp@gmail.com>
2016-05-02 16:17:33 +0430

    Example is generated and compiled.

--- examples/echoer/stub/echo_service_impl.cc
+++ examples/echoer/stub/echo_service_impl.cc
@@ -8,9 +8,9 @@
  
 #include <sstream>
 
-#include <naeem/hottentot/runtime/configuration.h>
-#include <naeem/hottentot/runtime/logger.h>
-#include <naeem/hottentot/runtime/utils.h>
+#include <org/labcrypto/hottentot/runtime/configuration.h>
+#include <org/labcrypto/hottentot/runtime/logger.h>
+#include <org/labcrypto/hottentot/runtime/utils.h>
 
 #include "echo_service_impl.h"
 
@@ -32,14 +32,18 @@ namespace echoer {
     // TODO: Called when service is shutting down.
   }
   void
-  EchoServiceImpl::EchoMessage(::ir::ntnaeem::hottentot::examples::echoer::RequestMessage &req, ::ir::ntnaeem::hottentot::examples::echoer::ResponseMessage &out) {
-    if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-      ::naeem::hottentot::runtime::Logger::GetOut() << "EchoServiceImpl::EchoMessage() is called." << std::endl;
+  EchoServiceImpl::EchoMessage (
+    ::ir::ntnaeem::hottentot::examples::echoer::RequestMessage &req, 
+    ::ir::ntnaeem::hottentot::examples::echoer::ResponseMessage &out,
+    ::org::labcrypto::hottentot::runtime::service::HotContext &hotContext
+  ) {
+    if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "EchoServiceImpl::EchoMessage() is called." << std::endl;
     }
     unsigned char *str = req.GetName().Serialize(NULL);
     std::stringstream ss;
     ss << "Hello " << str << "\r\n";
-    ::naeem::hottentot::runtime::types::Utf8String responseString(ss.str().c_str());
+    ::org::labcrypto::hottentot::Utf8String responseString(ss.str().c_str());
     out.SetMessage(responseString);
     delete [] str;
   }
--- examples/echoer/stub/echo_service_server.cc
+++ examples/echoer/stub/echo_service_server.cc
@@ -8,9 +8,9 @@
  
 #include <iostream>
 
-#include <naeem/hottentot/runtime/configuration.h>
-#include <naeem/hottentot/runtime/logger.h>
-#include <naeem/hottentot/runtime/service/service_runtime.h>
+#include <org/labcrypto/hottentot/runtime/configuration.h>
+#include <org/labcrypto/hottentot/runtime/logger.h>
+#include <org/labcrypto/hottentot/runtime/service/service_runtime.h>
 
 #include "../gen/request_message.h"
 #include "../gen/response_message.h"
@@ -21,21 +21,21 @@
 int
 main(int argc, char **argv) {
   try {
-    ::naeem::hottentot::runtime::Logger::Init();
-    ::naeem::hottentot::runtime::service::ServiceRuntime::Init(argc, argv);
-    if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-      ::naeem::hottentot::runtime::Logger::GetOut() << "Starting server ..." << std::endl;
+    ::org::labcrypto::hottentot::runtime::Logger::Init();
+    ::org::labcrypto::hottentot::runtime::service::ServiceRuntime::Init(argc, argv);
+    if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Starting server ..." << std::endl;
     }
     ::ir::ntnaeem::hottentot::examples::echoer::EchoServiceImpl *service =
         new ::ir::ntnaeem::hottentot::examples::echoer::EchoServiceImpl;
-    ::naeem::hottentot::runtime::service::ServiceRuntime::Register("0.0.0.0", 2000, service);
-    ::naeem::hottentot::runtime::service::ServiceRuntime::Start();
-    ::naeem::hottentot::runtime::service::ServiceRuntime::Shutdown();
-    if (::naeem::hottentot::runtime::Configuration::Verbose()) {
-      ::naeem::hottentot::runtime::Logger::GetOut() << "Service runtime is shutdown." << std::endl;
-      ::naeem::hottentot::runtime::Logger::GetOut() << "About to disable logging system ..." << std::endl;
+    ::org::labcrypto::hottentot::runtime::service::ServiceRuntime::Register("0.0.0.0", 2000, service);
+    ::org::labcrypto::hottentot::runtime::service::ServiceRuntime::Start();
+    ::org::labcrypto::hottentot::runtime::service::ServiceRuntime::Shutdown();
+    if (::org::labcrypto::hottentot::runtime::Configuration::Verbose()) {
+      ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "Service runtime is shutdown." << std::endl;
+      ::org::labcrypto::hottentot::runtime::Logger::GetOut() << "About to disable logging system ..." << std::endl;
     }
-    ::naeem::hottentot::runtime::Logger::Shutdown();
+    ::org::labcrypto::hottentot::runtime::Logger::Shutdown();
   } catch (...) {
     std::cout << "Error." << std::endl;
     return 1;
