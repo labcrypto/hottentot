4ec32031de25476dafe0726ee5d404e0021d5a58
Kamran Amini <kam.cpp@gmail.com>
2016-05-04 11:47:19 +0430

    Example java sources have been added.

---
+++ examples/echoer/java/hotgen/server/EchoImpl.java
@@ -0,0 +1,7 @@
+package ir.ntnaeem.hottentot.examples.echoer.hotgen.server;
+
+import ir.ntnaeem.hottentot.examples.echoer.hotgen.*;
+
+class EchoImpl extends AbstractEchoService {
+	//implementation
+}
