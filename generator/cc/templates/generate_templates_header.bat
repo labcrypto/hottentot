18e041bb679e425f67d1166f828d08a6a73e8f04
Kamran Amini <kam.cpp@gmail.com>
2016-10-11 11:58:10 +0330

    Windows CC tmeplate generation script is complete.

--- generator/cc/templates/generate_templates_header.bat
+++ generator/cc/templates/generate_templates_header.bat
@@ -3,3 +3,16 @@ echo. 2> %1\templates.cc
 for %%f in (".\*.template") do (
   %1\tools\xxd -i %%f >> %1\templates.cc
 )
+
+echo. 2> %1\templates.h
+
+echo #ifndef _NAEEM_HOTTENTOT_GENERATOR__TEMPLATES_H_ >> %1/templates.h
+echo #define _NAEEM_HOTTENTOT_GENERATOR__TEMPLATES_H_ >> %1/templates.h
+echo. >> %1/templates.h
+
+for /F "delims==" %%f in ('type templates.cc ^| find "unsigned"') do (
+  echo extern %%f; >> %1/templates.h
+)
+
+echo. >> %1/templates.h
+echo #endif >> %1/templates.h
